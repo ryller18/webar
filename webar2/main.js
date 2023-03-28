88c88a34b04b8b12933e01b852e6766471a391c3
ryller18 <44203373+ryller18@users.noreply.github.com>
2023-03-28 13:20:08 +0100

    Delete webar2 directory

--- webar2/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-  const model = document.querySelector("#model");
-
-  model.addEventListener("model-loaded", () => {
-    console.log("3D model loaded");
-  });
-});
