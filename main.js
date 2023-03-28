dfe36450a29e9ade39e75cbdc9f485dc8d5fae9d
ryller18 <44203373+ryller18@users.noreply.github.com>
2023-03-28 13:36:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,7 @@ window.addEventListener("DOMContentLoaded", function () {
   model.addEventListener("model-loaded", () => {
     console.log("3D model loaded");
   });
+  model.addEventListener("model-error", (event) => {
+    console.error("Error loading 3D model:", event.detail);
+  });
 });
